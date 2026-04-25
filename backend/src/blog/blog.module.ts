55b353ab8f7232ec680e234b6885ceec0d3a2f6f
Sajee <dilshanlive014@gmail.com>
2026-04-25 18:40:41 +0530

    blog types and modules

--- backend/src/blog/blog.module.ts
+++ backend/src/blog/blog.module.ts
@@ -1,10 +1,12 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { BlogRouter } from './blog.router';
+import { PrismaService } from '../prisma/prisma.service';
+import { BlogService } from './blog.service';
 import { TrpcModule } from '../trpc/trpc.module';
 
 @Module({
   imports: [forwardRef(() => TrpcModule)], // Import the module that has TrpcService
-  providers: [BlogRouter],
+  providers: [BlogRouter, BlogService, PrismaService],
   exports: [BlogRouter],
 })
 export class BlogModule {}

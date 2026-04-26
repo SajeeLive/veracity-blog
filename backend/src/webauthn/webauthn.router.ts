78e8230e769009b1b4191509172d6128d1541e6a
Sajee <dilshanlive014@gmail.com>
2026-04-26 22:44:49 +0530

    feat(webauthn): add verifyRegistration procedure to router

--- backend/src/webauthn/webauthn.router.ts
+++ backend/src/webauthn/webauthn.router.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { TrpcService } from '../trpc/trpc.service';
 import { WebauthnService } from './webauthn.service';
-import { getRegistrationOptionsSchema } from './webauthn.types';
+import {
+  getRegistrationOptionsSchema,
+  verifyRegistrationSchema,
+} from './webauthn.types';
 
 @Injectable()
 export class WebauthnRouter {
@@ -17,6 +20,14 @@ export class WebauthnRouter {
         .query(async ({ input }) => {
           return await this.webauthnService.getRegistrationOptions(
             input.handle,
+          );
+        }),
+      verifyRegistration: this.trpc.procedure
+        .input(verifyRegistrationSchema)
+        .mutation(async ({ input }) => {
+          return await this.webauthnService.verifyRegistration(
+            input.handle,
+            input.response,
           );
         }),
     });

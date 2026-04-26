d3b751391d08c9f90b23cec4dec5b5e50f88ece7
Sajee <dilshanlive014@gmail.com>
2026-04-26 16:10:09 +0530

    feat(backend): register webauthn configuration loader in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TrpcModule } from './trpc/trpc.module';
 import {BlogModule}  from './blog/blog.module';
 import { envSchema } from './config/env.validation';
+import webauthnConfig from './config/webauthn.config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -12,6 +13,7 @@ import { AppService } from './app.service';
     ConfigModule.forRoot({
       isGlobal: true, // No need to import into other modules
       validate: (config) => envSchema.parse(config),
+      load: [webauthnConfig],
       envFilePath: '.env',
       expandVariables: true,
     }),
